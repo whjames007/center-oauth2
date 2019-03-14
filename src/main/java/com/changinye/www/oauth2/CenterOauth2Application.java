ba53eab719c7663c4ebbad65e6691db2925d9aa5
丁丁历险记 <james.song007@hotmail.com>
2019-03-14 17:10:58 +0800

    114

--- src/main/java/com/changinye/www/oauth2/CenterOauth2Application.java
+++ src/main/java/com/changinye/www/oauth2/CenterOauth2Application.java
@@ -1,13 +1,21 @@
 package com.changinye.www.oauth2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CenterOauth2Application {
+
+	private static Logger logger = LoggerFactory.getLogger(CenterOauth2Application.class);
+	private static String name = "【Oauth2安全认证中心】";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CenterOauth2Application.class, args);
+		logger.info("【启动微服务成功】{}", name);
 	}
 
 }
---
+++ src/main/java/com/changinye/www/oauth2/model/UserInfo.java
@@ -0,0 +1,57 @@
+package com.changinye.www.oauth2.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserInfo implements UserDetails{
+	
+	private static final long serialVersionUID = -7990600170411641239L;
+
+	private String userAccount;
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
