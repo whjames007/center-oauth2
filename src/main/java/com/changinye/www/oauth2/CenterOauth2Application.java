52795d60b8e4e884644904df242c9a05a258f60b
丁丁历险记 <james.song007@hotmail.com>
2019-03-14 15:57:24 +0800

    Initial commit

---
+++ src/main/java/com/changinye/www/oauth2/CenterOauth2Application.java
@@ -0,0 +1,13 @@
+package com.changinye.www.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterOauth2Application.class, args);
+	}
+
+}
